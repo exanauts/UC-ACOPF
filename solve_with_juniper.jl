e1aab6c647f15a33dcd0349c666ce0ecdfb2670b
wzhangw <weiqizhang64@gmail.com>
2023-06-26 05:18:58 +0000

    run juniper with v2 case

--- solve_with_juniper.jl
+++ solve_with_juniper.jl
@@ -5,23 +5,27 @@ using Ipopt, HiGHS
 include("MPOPF-sampler/src/mpc_data.jl")
 include("jump_models.jl")
 
-case = "case9"
-# case = "case118"
+# case = "case9"
+case = "case118"
+# case = ARGS[1]
 gen_scenario = "$(case)_gen"
 
-T = 6
-circuit = getcircuit("data/$(case)", 100, 0.1)
-load = getload("data/multiperiod_data/$(case)_onehour_168", circuit)
-# load.pd ./= 3
-# load.qd ./= 3
+T = 24
+# T = parse(Int, ARGS[2])
 
-v0 = readdlm("data/multiperiod_data/$(gen_scenario).v0", Int)
-hu = readdlm("data/multiperiod_data/$(gen_scenario).hu", Int)
-hd = readdlm("data/multiperiod_data/$(gen_scenario).hd", Int)
-tu = readdlm("data/multiperiod_data/$(gen_scenario).tu", Int)
-td = readdlm("data/multiperiod_data/$(gen_scenario).td", Int)
-con = readdlm("data/multiperiod_data/$(gen_scenario).con")
-coff = readdlm("data/multiperiod_data/$(gen_scenario).coff")
+println("============================================")
+println("Solving $(case) with $(T) hours...")
+
+circuit = getcircuit("data_v2/$(case)", 100, 0.1)
+load = getload("data_v2/multiperiod_data/$(case)_onehour_168", circuit)
+
+v0 = readdlm("data_v2/multiperiod_data/$(gen_scenario).v0", Int)
+hu = readdlm("data_v2/multiperiod_data/$(gen_scenario).hu", Int)
+hd = readdlm("data_v2/multiperiod_data/$(gen_scenario).hd", Int)
+tu = readdlm("data_v2/multiperiod_data/$(gen_scenario).tu", Int)
+td = readdlm("data_v2/multiperiod_data/$(gen_scenario).td", Int)
+con = readdlm("data_v2/multiperiod_data/$(gen_scenario).con")
+coff = readdlm("data_v2/multiperiod_data/$(gen_scenario).coff")
 
 
 model = get_ucmodel(circuit, load, T, v0, tu, td, hu, hd, con, coff)
@@ -34,4 +38,7 @@ minlp_solver = optimizer_with_attributes(Juniper.Optimizer, "nl_solver"=>nl_solv
 
 set_optimizer(model, minlp_solver)
 
-optimize!(model)
+@time optimize!(model)
+
+println("Termination status: ", termination_status(model))
+println("============================================")
